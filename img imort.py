f09cf77ea94863b8654d80a3ab422bea0886211a
maria <60131841+maria546@users.noreply.github.com>
2020-02-20 22:38:00 -0800

    Update img imort.py

--- img imort.py
+++ img imort.py
@@ -1,5 +1,5 @@
-import cv2
-img = cv2.imread("panda.jpg",1)
+import cv2                                   #first install (pip install opencv-python)
+img = cv2.imread("panda.jpg",1)               # Then import cv2 and type this code
 cv2.imshow("pic",img)
 cv2.waitKey()
 cv2.destroyAllWindows()
